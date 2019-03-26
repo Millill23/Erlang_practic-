91af5af087784169398ae1a64a1578e16cc818d0
Millill23 <48770464+Millill23@users.noreply.github.com>
2019-03-26 18:40:51 +0300

    Update task_5.erl

--- 02_list_recursion/exercise/task_5.erl
+++ 02_list_recursion/exercise/task_5.erl
@@ -7,8 +7,15 @@
 
 %% implement lists:splitwith/2
 %% http://www.erlang.org/doc/man/lists.html#splitwith-2
-splitwith(Pred, List) ->
-    {[], []}.
+splitwith(Pred, List) -> splitwith(Pred,List,[]).
+
+
+splitwith(_Pred, [],Acc) -> {[], []};
+splitwith(Pred,[Head | Tail],Acc) ->
+  case Pred(Head) of
+    true -> splitwith(Pred, Tail,[Head | Acc]);
+    false -> {task_2:reverse(Acc), [Head | Tail]}
+  end.
 
 
 splitwith_test() ->
@@ -25,8 +32,11 @@ splitwith_test() ->
 %% implement lists:zipwith/3
 %% http://www.erlang.org/doc/man/lists.html#zipwith-3
 %% if two lists have different lengths don't throw exception but ignore the rest of longer list
-zipwith(Pred, List1, List2) ->
-    [].
+zipwith(Pred, List1, List2) -> zipwith(Pred, List1, List2, []).
+
+zipwith(_Pred,[],_List2,Acc) -> task_2:reverse(Acc);
+zipwith(_Pred,_List1,[],Acc) -> task_2:reverse(Acc);
+zipwith(Pred,[H1 | T1],[H2 | T2], Acc) -> zipwith(Pred, T1, T2, [Pred(H1,H2) | Acc]).
 
 
 zipwith_test() ->
@@ -38,4 +48,4 @@ zipwith_test() ->
     ?assertEqual([2,4,6,8,10], zipwith(F, [1,2,3,4,5], [1,2,3,4,5])),
     ?assertEqual([2,4,6,8], zipwith(F, [1,2,3,4], [1,2,3,4,5,6])),
     ?assertEqual([2,4,6,8], zipwith(F, [1,2,3,4,5,6], [1,2,3,4])),
-    ok.
+ok.
