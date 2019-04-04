c607edc2089d3e09b39b061c9219cd705e3d1724
Millill23 <48770464+Millill23@users.noreply.github.com>
2019-04-04 13:27:19 +0300

    Update template.erl

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -3,4 +3,17 @@
 -export([parse/2]).
 
 parse(Str, Data) when is_binary(Str) ->
-    Str.
+  Parts = binary:split(Str,[<<"{{">>],[global]),
+  parse(Parts,Data,[]).
+
+parse([],_,Acc) -> unicode:characters_to_binary(lists:reverse(Acc));
+parse([Part|Tail],Data,Acc)->
+  case binary:split(Part,[<<"}}">>]) of
+    [Any] -> parse(Tail,Data,[Any|Acc]);
+    [NameValue|Any] ->
+      case maps:find(NameValue,Data) of
+        {ok,Value} when is_binary(Value);is_list(Value) -> parse(Tail,Data,[[Value|Any]|Acc]);
+        {ok,Value} when is_integer(Value) -> parse(Tail,Data,[[integer_to_binary(Value)|Any]|Acc]);
+        error -> parse(Tail,Data,[Any|Acc])
+      end
+    end.
