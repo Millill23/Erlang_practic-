34423a4674bf17c44e9971c6aea5170c25c8d890
Millill23 <48770464+Millill23@users.noreply.github.com>
2019-04-11 19:49:06 +0300

    Update worker.erl

--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,12 @@
 -module(worker).
+
+-export([run/2]).
+
+run(ReducePid,File)->
+  {ok,Binary} = file:read_file(File),
+  Items = binary:split(Binary,[<<"\n">>],[global]),
+  F =   fun(<<>>,Acc) -> Acc;
+           (Item,Acc) -> [_ID,Name,Count,_Price] = binary:split(Item,[<<",">>],[global]),
+                         Acc#{Name => list_to_integer(binary_to_list(Count))} end,
+  Result = lists:foldl(F,#{},Items),
+  ReducePid ! {result,Result}.
