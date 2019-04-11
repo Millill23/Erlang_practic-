e93aafb97cd297fc256507f44e8a5f19a6ab17fe
Millill23 <48770464+Millill23@users.noreply.github.com>
2019-04-11 20:40:10 +0300

    Update main_sup.erl

--- 12_supervisor/exercise/main_sup.erl
+++ 12_supervisor/exercise/main_sup.erl
@@ -2,4 +2,26 @@
 
 -export([start_link/0, init/1]).
 
-%% TODO
+start_link() ->
+    supervisor:start_link({local,?MODULE},?MODULE, []).
+
+
+init([]) ->
+    SupervisorSpecification = #{strategy => one_for_one,
+                                intensity => 1,
+                                period => 1000},
+    Sup1 =  #{id => sup_1,
+                 start => {sup_1, start_link, []},
+                 restart => permanent,
+                 shutdown => 1000,
+                 type => supervisor,
+                 modules => [sup_1]
+                },
+    Sup2 =  #{id => sup_2,
+                 start => {sup_2, start_link, []},
+                 restart => permanent,
+                 shutdown => 1000,
+                 type => supervisor,
+                 modules => [sup_2]
+                },
+{ok, {SupervisorSpecification, [Sup1, Sup2]}}.
